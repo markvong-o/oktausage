0e88d703b981853760f0edae51871ce7b9511e2e
markvong-o <83755325+markvong-o@users.noreply.github.com>
2024-06-10 21:21:14 -0700

    Set to filter unique ids

--- app/api/syslog/helpers.ts
+++ app/api/syslog/helpers.ts
@@ -1,4 +1,4 @@
-import parse from 'parse-link-header';
+import parse from "parse-link-header";
 
 export async function getLogData(
   domain: string,
@@ -8,13 +8,13 @@ export async function getLogData(
   until?: string
 ) {
   let url = `https://${domain}/api/v1/logs?since=${since}&until=${until}&filter=${
-    filter ? filter : ''
+    filter ? filter : ""
   }&limit=1000`;
   const getOptions = {
-    method: 'GET',
+    method: "GET",
     headers: {
       Authorization: `SSWS ${apiKey}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
   const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
@@ -24,17 +24,17 @@ export async function getLogData(
   if (res.status === 401) {
     throw new Error(`${res.statusText}! Check your API Token`);
   }
-  if(res.status === 400) {
+  if (res.status === 400) {
     res = await res.json();
     throw new Error(`${res.errorSummary}`);
   }
   let headers = res.headers;
   res = await res.json();
   logs.push(...res);
-  let links = headers.get('link');
+  let links = headers.get("link");
   let parsedLinks = parse(links);
-  let rateLimitRemaining = parseInt(headers.get('x-rate-limit-remaining')!);
-  let hasNext = parsedLinks && 'next' in parsedLinks;
+  let rateLimitRemaining = parseInt(headers.get("x-rate-limit-remaining")!);
+  let hasNext = parsedLinks && "next" in parsedLinks;
   let nextLink = parsedLinks?.next?.url;
 
   let counter = 0;
@@ -47,14 +47,14 @@ export async function getLogData(
     if (res.length === 0) {
       break;
     }
-    rateLimitRemaining = parseInt(headers.get('x-rate-limit-remaining')!) || 0;
+    rateLimitRemaining = parseInt(headers.get("x-rate-limit-remaining")!) || 0;
     if (rateLimitRemaining < 30) {
       await sleep(1500);
     }
     logs.push(...res);
-    links = headers.get('link');
+    links = headers.get("link");
     parsedLinks = parse(links);
-    hasNext = parsedLinks && 'next' in parsedLinks;
+    hasNext = parsedLinks && "next" in parsedLinks;
     if (parsedLinks?.next?.url === nextLink) {
       break;
     }
@@ -88,7 +88,6 @@ export async function getUniqueUsers(
   apiKey: string,
   days?: number
 ) {
-  // let filter: string = `eventType eq "app.oauth2.token.grant.id_token" and outcome.result eq "SUCCESS"`;
   let filter: string = `eventType eq "user.authentication.sso" or eventType eq "user.session.start" or eventType eq "app.oauth2.as.authorize.implicit.id_token" or eventType eq "app.oauth2.as.token.grant.id_token" or eventType eq "app.oauth2.as.token.grant.refresh_token" or eventType eq "app.oauth2.authorize.implicit.id_token" or eventType eq "app.oauth2.authorize.implicit.access_token" or eventType eq "app.oauth2.token.grant.access_token" or eventType eq "app.oauth2.token.grant.id_token" or eventType eq "app.oauth2.token.grant.refresh_token" or eventType eq "app.oauth2.as.authorize.implicit.access_token" or eventType eq "app.oauth2.as.token.grant.access_token") and outcome.result eq "SUCCESS" and (debugContext.debugData.grantType ne "client_credentials" or debugContext.debugData.grantType eq "NULL")`;
 
   try {
@@ -104,10 +103,9 @@ export async function getUniqueUsers(
       .map((targets) => targets[0])
       .map((target) => target.id);
 
-    let uniqueActorIds = actorIds.filter(
-      (ele, idx) => actorIds.indexOf(ele) === idx
-    );
-    return uniqueActorIds.length;
+    let uniqueActorIds = new Set(actorIds);
+
+    return uniqueActorIds.size;
   } catch (e) {
     throw e;
   }
